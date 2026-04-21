c7631d7ffbee18b7edbfc79105c67a37b088ff2c
Paul <gutibj44@gmail.com>
2026-04-21 15:53:29 +0800

    feat: implement navigation stack with authentication routing

---
+++ src/navigations/AuthNav.js
@@ -0,0 +1,26 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { ROUTES } from '../utils';
+
+// screens
+import Login from '../screens/auth/Login';
+import Register from '../screens/auth/Register';
+import Error from '../screens/auth/ErrorScreen';
+
+const Stack = createStackNavigator();
+
+const AuthNavigation = () => {
+    return (
+        <Stack.Navigator initialRouteName={ROUTES.LOGIN}>
+        <Stack.Screen
+            name={ROUTES.LOGIN}
+            component={Login}
+            options={{
+            headerShown: false,
+            }}
+        />
+        <Stack.Screen name={ROUTES.REGISTER} component={Register} />
+        </Stack.Navigator>
+    );
+};
+
+export default AuthNavigation;
--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,10 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
-import MainNavigation from './MainNav';
+import { useEffect } from 'react';
+import { Platform, StatusBar, useColorScheme } from 'react-native';
+import { useSelector } from 'react-redux';
 
-export default function AppNav() {
+import AuthNav from './AuthNav';
+import MainNav from './MainNav';
+
+export default () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const auth = useSelector(state => state.auth);
+  const isLoggedIn = !!auth?.data;
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      StatusBar.setBarStyle('dark-content', true);
+    }
+  }, [isDarkMode]);
+
   return (
     <NavigationContainer>
-      <MainNavigation />
+      {isLoggedIn ? <MainNav /> : <AuthNav />}
     </NavigationContainer>
   );
-}
+};
